c99f22fdb1b374bc055788b2a45c0a605405f858
jefer10 <jefersson290@hotmail.com>
2019-04-03 12:07:30 -0500

    se realiza la identificacion de fonemas no vocalizados

--- procesamiento_fonemas.m
+++ procesamiento_fonemas.m
@@ -22,7 +22,6 @@ for i=1:NW
     E(i)=y'*y/WL;%saca energia de ventana
     Z(i)=sum(abs(diff(sign(y))))/WL/2;%saca energia de ventana
 end
-
 t=(0:L-1)'/fs;
 t1=(floor(WL/2)+WD*(0:NW-1)')/fs;%saca las partes en x de ventana
 E=10*log10(E);%pasa energia a dB
@@ -35,6 +34,8 @@ plot(t1,E,'Color','blue')
 plot(t1,Z,'Color','black')
 legend('x','E','Z');
 
+
+
 silencio=zeros(length(Z),1);
 if((E(1)<0.2)&&(Z(1)>0.33))
     silencio(1)=1;
@@ -43,21 +44,44 @@ else
 end
 for j=1:length(Z)-1
     if(E(j)<0.216)
-        if((Z(j)>Z(j+1))&&(Z(j+1)<0.18))%anterior mayor que la presente 
+        if(((Z(j)>Z(j+1))&&(Z(j+1)<0.18)))% || ((Z(j)<Z(j+1)) && (Z(j+1)<0.7)))%anterior mayor que la presente 
             silencio(j+1)=0;%Se apaga
         elseif ((Z(j)<Z(j+1))&&(Z(j+1)>0.33))%la anterior menor que la presente
             silencio(j+1)=1;%prende            
         else
-            silencio(j+1)=silencio(j);%mantiene
+            silencio(j+1)=silencio(j);%se mantiene sobre la anterior 
         end        
     else
         silencio(j)=0;
     end
 end
+%
+
+no_vocalizados=zeros(length(Z),1);
+if ((Z(1)>0.7) && (E(1))<0.5)
+    no_vocalizados(1)=1;
+else
+    no_vocalizados(1)=0;
+end
+for k=1:length(Z)
+    if(Z(k)>0.6)
+        if((E(k)<E(k+1)) && (E(k+1)>0.51)&&(E(k+1)<0.7))
+            no_vocalizados(k+1)=1;
+        elseif(((E(k)>E(k+1)) && (E(k+1)<0.45))||((E(k)<E(k+1))&&(E(k+1)>0.7)))
+            no_vocalizados(k+1)=0;
+        else
+            no_vocalizados(k+1)=no_vocalizados(k);
+        end
+    else
+        no_vocalizados(k)=0;
+    end
+    
+end
 subplot(2,1,2);
-plot(t1,silencio,'Color','blue')
 hold on;
+plot(t1,no_vocalizados,'Color','blue');
+plot(t1,silencio,'Color','black');
 plot(t,x,'Color','red')
-legend('x','Silencios');
+legend('no vocalizados','silencio','x');
 ax = gca;
-ax.YLim = [0 1.2]; 
+ax.YLim = [0 1.2];
