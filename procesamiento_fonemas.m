f706523c6cf94a517dcc9ebab40d459e82c8f65a
nickdpz <ndpastranzamora@gmail.com>
2019-04-07 21:13:09 -0500

    Algoritmo adaptado a señal de celular

--- procesamiento_fonemas.m
+++ procesamiento_fonemas.m
@@ -1,17 +1,19 @@
 clear all;
 close all;
-[x,fs]=audioread('vozfemenina.wav');
+[x1,fs]=audioread('vozkatheinf.wav');
+x=x1(:,2);
+%[x,fs]=audioread('vozfemenina.wav');
 L=length(x);
 t=(0:L-1)'/fs;
-plot(t,x)
-s=x(600+(0:511));
-e=x(1650+(0:511));
-si=x(17500+(0:511));
-ES=s'*s/512;
-Ee=e'*e/512;
-zs=sum(abs(diff(sign(s))))/512/2;
-ze=sum(abs(diff(sign(e))))/512/2;
-zsi=sum(abs(diff(sign(si))))/512/2;
+x=x/(abs(min(x)));
+%s=x(600+(0:511));
+%e=x(1650+(0:511));
+%si=x(17500+(0:511));
+%ES=s'*s/512;
+%Ee=e'*e/512;
+%zs=sum(abs(diff(sign(s))))/512/2;
+%ze=sum(abs(diff(sign(e))))/512/2;
+%zsi=sum(abs(diff(sign(si))))/512/2;
 WL=256;
 WD=8;
 NW=floor((L-WL)/WD);%Obtiene numero de Ventanas
@@ -19,12 +21,11 @@ E=zeros(NW,1);%Declara arreglo
 Z=zeros(NW,1);%Declara arreglo
 for i=1:NW
     y=x((i-1)*WD+(1:WL));%asigna la ventana 
-    E(i)=y'*y/WL;%saca energia de ventana
+    E(i)=((y')*y)/WL;%saca energia de ventana
     Z(i)=sum(abs(diff(sign(y))))/WL/2;%saca energia de ventana
 end
 t=(0:L-1)'/fs;
 t1=(floor(WL/2)+WD*(0:NW-1)')/fs;%saca las partes en x de ventana
-E=10*log10(E);%pasa energia a dB
 E=(E-min(E))/(max(E)-min(E));%Ubica en el rango 0 a 1
 Z=(Z-min(Z))/(max(Z)-min(Z));
 %subplot(2,1,1);
@@ -32,10 +33,11 @@ hold on
 plot(t,x,'Color','red')
 plot(t1,E,'Color','blue')
 plot(t1,Z,'Color','black');
-%legend('x','E','Z');
+legend('x','E','Z');
+%%
 y=0.18+t1*0;
 y2=0.35+t1*0;
-plot(t1,y,'--black',t1,y2,'--black');%Comentar para quitar los tramos 
+%plot(t1,y,'--black',t1,y2,'--black');%Comentar para quitar los tramos 
 silencio=zeros(length(Z),1);
 if((E(1)<0.2)&&(Z(1)>0.32))
     silencio(1)=1;
