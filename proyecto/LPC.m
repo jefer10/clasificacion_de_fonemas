bc4df1dce55f995a193ef96ba4925323a5405eed
nickdpz <ndpastranzamora@gmail.com>
2019-07-06 19:01:25 -0500

    Crea algoritmo para leer base de datos

--- proyecto/LPC.m
+++ proyecto/LPC.m
@@ -1,6 +1,7 @@
 clc ;clear all;close all;
-[x1,fs]=audioread('A.wav');
-x=x1(:,1);
+[x1,fs]=audioread('aaaas.wav');
+x=x1((133179:162401),1);
+%%plot(x1)
 l = length(x);        % Length of signal
 t=(0:l-1)'/fs;
 wl=128;
@@ -9,11 +10,10 @@ tiempo_total=length(t);
 nw=floor((l-wl)/wd);
 tiempo_ventana=(tiempo_total/nw)/fs;
 f = fs*(0:(wl/2))/wl;
-
 %%construccion matriz autocorrelacion y coeficientes de autocorrelacion
 p = 6; %% polos...frecuancias resonancia
 r = zeros(p+1,1);
-V=zeros(6,nw);
+V1=zeros(6,nw);
 for i=1:nw %i en tiempo
 %%filtro pre enfasis%%enfatizar frecuencias altas%%mirar frecuencias
 %%resonantes
@@ -34,5 +34,12 @@ for j=2:p
 end
 %%estimadores coeficientes filtros digital
 a= -inv(R)*r; 
-V(:,i)=a;
+V1(:,i)=a;
 end
+%%
+save BaseA.mat;%Crea el archivo de la base de datos
+BaseAObject = matfile('BaseA.mat','Writable',true);
+%%
+load('BaseA.mat','V');%carga la base de datos
+V=[V,V1];% concatena datos
+save('BaseA.mat','-append','V');%guarda nuevos datos
